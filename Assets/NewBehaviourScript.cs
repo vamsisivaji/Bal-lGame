34baad742664c650f4206cd5ffe98f8898ab51ff
vamsisivaji <38399482+vamsisivaji@users.noreply.github.com>
2021-03-02 16:27:51 +0530

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horiontalMovement, 0, verticalMovement);
+        rb.AddForce(movement * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/wallCollision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wallCollision : MonoBehaviour
+{
+    public Material[] wallMat;
+    Renderer rend;
+    public Text display;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        display.text = "Roll the Ball";
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name=="Ball")
+        {
+            rend.sharedMaterial = wallMat[1];
+            display.text = "u Hit the Wall";
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            rend.sharedMaterial = wallMat[0];
+            display.text = "Keep Rolling the ball";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
